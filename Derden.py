62a28a9cef3890512a90c2045559761c0afd62c6
yngh0m1e <140334733+yngh0m1e@users.noreply.github.com>
2023-08-11 03:36:35 +0600

    Create Derden.py

---
+++ Derden.py
@@ -0,0 +1,24 @@
+import discord
+import random
+from gen import gen_pass
+client = discord.Client(intents=discord.Intents.all())
+
+game = ['Орёл', 'Решка']
+
+@client.event
+async def on_ready(): #функция, отражающая готовность бота к работе
+    print(f'Вы зареганы {client.user}')
+    print('Ваш пароль для входа', gen_pass(10))
+
+@client.event
+async def on_message(message): #Направлена на общение с пользователем
+    if message.author == client.user:
+        return
+    if message.content.startswith('!hello'):
+        await message.channel.send('hi!')
+    if message.author == client.user:
+        return
+    if message.content.startswith('!Game'):
+        await message.channel.send(random.choice(game))
+
+client.run('MTEzNTE1NDgyNzUwNTE3MjQ5MA.Gv78z0.QdbbKb4ki64EkP5xP6ZT9LhhQAmW7n3y90LHeM')
