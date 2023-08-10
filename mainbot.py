59cc1194525c50c622112f0d9462f3868f8650c0
yngh0m1e <140334733+yngh0m1e@users.noreply.github.com>
2023-08-11 03:38:03 +0600

    Create mainbot.py

---
+++ mainbot.py
@@ -0,0 +1,25 @@
+import discord
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("ТУТ ДОЛЖЕН БЫТЬ TOKEN ТВОЕГО БОТА")
